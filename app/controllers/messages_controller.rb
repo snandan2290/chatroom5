b3aaa99bfff4b4158c752db825e51c244bbf2818
VnandanS <vidyanandan.s@moviuscorp.com>
2023-12-19 20:45:22 +0530

    USing actioncables for fullduplex communication

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,9 @@ class MessagesController < ApplicationController
       message = current_user.messages.build(message_params)
       Rails.logger.debug("Message::message::#{message.inspect}")
       if message.save
-        redirect_to root_path
+        ActionCable.server.broadcast "chatroom_channel", 
+                                      mod_message: message_render(message)
+        # redirect_to root_path
       end
     end
 
@@ -16,6 +18,10 @@ class MessagesController < ApplicationController
     def message_params
       Rails.logger.debug("::::#{params.fetch(:message,{}).permit(:message_body)}")
       params.fetch(:message,{}).permit(:message_body)
+    end
+
+    def message_render(message)
+      render(partial: 'message', locals: {message: message})
     end
 
 end
